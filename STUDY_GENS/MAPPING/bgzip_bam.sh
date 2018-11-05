1786c0ce5c39f5d534e40f2561e8400281b237d1
Alejandro Valenzuela Seba <avalenzu@login2.cnag>
2018-11-05 16:14:31 +0100

    NOT bgzip the bam file and remove duplicate from our chimpanzee individual samples

---
+++ STUDY_GENS/MAPPING/bgzip_bam.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+module load gcc/4.9.3
+module load PYTHON/3.6.3
+
+
+#chimp folders
+chimp_names="central-Nico verus-McVean schweinfurthii-A912_Nakuu ellioti-Paquita" 
+
+  
+#BAM INPUT
+DATA="/scratch/devel/avalenzu/Impute_Master_Project/data/STUDY_GENS/"
+INDIR=${DATA}"BAM/"
+
+#FIRST WE NEED TO CHANGE THE NAME FOR Verus and Central to merged bam file
+
+
+#MAIN SCRIPT LOOPED FOR 3 CHIMPS
+
+echo $chimp_names | tr " " "\n" | while read chimp_name;
+do echo "#!/bin/bash
+module purge
+module load gcc/4.9.3-gold
+module load xz/5.2.2
+module load SAMTOOLS/1.3
+cd ${INDIR}Pan_troglodytes_${chimp_name}; bgzip *.merged.bam" > ${INDIR}Pan_troglodytes_${chimp_name}/qu/${chimp_name}_bgzip.sh
+jobname=$(echo ${INDIR}Pan_troglodytes_${chimp_name}/qu/${chimp_name}_bgzip.sh)
+chmod 755 $jobname
+/scratch/devel/avalenzu/CNAG_interface/submit.py -c ${jobname} -o ${INDIR}Pan_troglodytes_${chimp_name}/out/${chimp_name}.bgzip.out \
+-e ${INDIR}Pan_troglodytes_${chimp_name}/out/${chimp_name}.bgzip.err -n ${chimp_name} -u 1 -t 1 -w 03:00:00
+done
+
+
+
+
+
+
---
+++ STUDY_GENS/MAPPING/rm_dup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+module load gcc/4.9.3
+module load PYTHON/3.6.3
+
+#chimp folders
+chimp_names="central-Nico verus-McVean schweinfurthii-A912_Nakuu ellioti-Paquita" 
+#We need to have the files from sorted bam with merged name
+  
+#BAM OUTPUT
+DATA="/scratch/devel/avalenzu/Impute_Master_Project/data/STUDY_GENS/"
+INDIR=${DATA}"BAM/"
+OUTDIR=${DATA}"BAM_RMDUP/"
+
+
+#MAIN SCRIPT LOOPED FOR 3 CHIMPS
+
+mkdir -p ${OUTDIR}
+echo $chimp_names | tr " " "\n" | while read chimp_name;
+do mkdir -p ${OUTDIR}Pan_troglodytes_${chimp_name}
+mkdir -p ${OUTDIR}Pan_troglodytes_${chimp_name}/qu/
+mkdir -p ${OUTDIR}Pan_troglodytes_${chimp_name}/out/
+mkdir -p ${OUTDIR}Pan_troglodytes_${chimp_name}/tmp/
+in_file=$(ls ${INDIR}Pan_troglodytes_${chimp_name}/*.merged.bam | tr " " "\n" | rev | cut -d/ -f1 | rev | tr "\n" " ")
+input=$(echo $in_file)
+echo $in_file
+echo $input
+echo "#!/bin/bash
+module purge
+module load gcc/4.9.3-gold
+module load xz/5.2.2
+module load SAMTOOLS/1.3
+module load java
+module load PICARD/2.8.2
+
+#MAIN SCRIPT
+java -Djava.io.tmpdir=${OUTDIR}Pan_troglodytes_${chimp_name}/tmp/ -jar /apps/PICARD/2.8.2/picard.jar MarkDuplicates I=${INDIR}Pan_troglodytes_${chimp_name}/$input O=${OUTDIR}Pan_troglodytes_${chimp_name}/${chimp_name}_rmdup.bam M=${OUTDIR}Pan_troglodytes_${chimp_name}/${chimp_name}_metrics.txt REMOVE_DUPLICATES=true TMP_DIR=${OUTDIR}Pan_troglodytes_${chimp_name}/tmp/working_tmp;
+samtools index ${OUTDIR}Pan_troglodytes_${chimp_name}/${chimp_name}_rmdup.bam " > ${OUTDIR}Pan_troglodytes_${chimp_name}/qu/${chimp_name}_rmdup.sh
+jobname=$(echo ${OUTDIR}Pan_troglodytes_${chimp_name}/qu/${chimp_name}_rmdup.sh)
+chmod 755 $jobname
+
+/scratch/devel/avalenzu/CNAG_interface/submit.py -c ${jobname} -o ${OUTDIR}Pan_troglodytes_${chimp_name}/out/${chimp_name}.rmdup.out \
+-e ${OUTDIR}Pan_troglodytes_${chimp_name}/out/${chimp_name}.rmdup.err -n ${chimp_name} -u 8 -t 1 -w 06:00:00
+done
+
+
+
