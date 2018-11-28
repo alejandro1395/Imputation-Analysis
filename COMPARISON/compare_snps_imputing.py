8516eaf95d9f72b394dd13b11052fe0fdd0ed67d
alejandro1395 <a.vale_13@icloud.com>
2018-11-28 16:59:52 +0100

    Update compare_snps_imputing.py

--- COMPARISON/compare_snps_imputing.py
+++ COMPARISON/compare_snps_imputing.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+
+"""
+Script for comparing genotypes between the reference high coverage vcf for our chimp and chromosome and 
+the output snps of the imputation process carried out by Impute2
+"""
+
+
 import sys
 import re
 import gzip
@@ -14,9 +21,13 @@ else:
     sys.exit("The usage should be ./compare_snps_imputing.py \
     gen_input_file info_input_name ref_file threeshold out_file")
 
+#DICTIONARIES
+
 dict_known = {}
 dict_well_imputed = {}
 dict_impute = {}
+
+#VARIABLES
 
 corrected_known = 0
 total_known = 0
@@ -25,40 +36,91 @@ total_imputed = 0
 total_impute = 0
 found_impute = 0
 
+#FUNCTIONS
+
+"""
+Function for saving all information coming
+from gen file and info file which result from the IMPUTE2 analysis
+on our data
+"""
+
+def retrieve_snp_fields_from_gen_file(first_line, second_line):
+    fields1 = first_line.rstrip().split()
+    fields2 = second_line.rstrip().split()
+    chrom = int(fields1[0])
+    Ident = fields1[1]
+    position = fields1[2]
+    reference = fields1[4]
+    if reference == "0":
+        reference = "."
+    alternative = fields1[3]
+    if alternative == "0":
+        alternative = "."
+    Info = float(fields2[6])
+    Type = int(fields2[8])
+    return chrom, Ident, position, reference, alternative, Info, Type
+
+"""
+Function for analyzing the different genotype probbilities and choosing the best one for
+comparing it afterwars with the reference high-coverage vcf from the same individual chimp
+"""
+
+
+def find_best_genotype(first_line):
+    fields_prob = first_line.rstrip().split()
+    probabilities = [float(fields_prob[7]), float(fields_prob[6]), float(fields_prob[5])]
+    maximum_prob = max(probabilities)
+    for i, j in enumerate(probabilities):
+        if j == maximum_prob:
+            max_index = i
+    if max_index == 0:
+        GT = "0/0"
+    elif max_index == 1:
+        GT = "0/1"
+    elif max_index == 2:
+        GT = "1/1"
+    return GT
+
+
+"""
+Function for keeping information coming from the reference VCF of the individual chimp
+to compare it
+"""
+
+def retrieve_ref_vcf_info(line1):
+    fields = line1.rstrip().split()
+    posi = fields[1]
+    refe = fields[3]
+    alte_list = fields[4].split(",")
+    gt_refe = fields[5]
+    gt_alte = fields[6]
+    return posi, refe, alte_list, gt_refe, gt_alte
+
+"""
+Function to compare the genotypes between vcf ref and vcf impute2 output and
+count the similar ones and the different ones
+"""
+
+def compare_gt(dictionary, corrected_dictionary, identifier, gt_refe, gt_alte):
+    GT_a0 = dictionary[identifier].split("/")[0]
+    GT_a1 = dictionary[identifier].split("/")[1]
+    if GT_a0 == gt_refe and GT_a1 == gt_alte:
+        corrected_dictionary += 1
+    return corrected_dictionary
+
+#MAIN
+
 with gzip.open(gen_input_file, "rt") as f1, \
     open(info_input_name, "r") as f2:
         next(f2)
         for line1, line2 in zip(f1, f2):
-            fields1 = line1.rstrip().split()
-            fields2 = line2.rstrip().split()
-            chromosome = int(fields1[0])
-            ID = fields1[1]
-            pos = fields1[2]
-            ref = fields1[4]
-            if ref == "0":
-                ref = "."
-            alt = fields1[3]
-            if alt == "0":
-                alt = "."
-            probabilities = [float(fields1[7]), float(fields1[6]),
-                             float(fields1[5])]
-            maximum_prob = max(probabilities)
-            for i, j in enumerate(probabilities):
-                if j == maximum_prob:
-                    max_index = i
-            if max_index == 0:
-                GT = "0/0"
-            elif max_index == 1:
-                GT = "0/1"
-            elif max_index == 2:
-                GT = "1/1"
-            INFO = float(fields2[6])
-            TYPE = int(fields2[8])
+            chromosome, ID, pos, ref, alt, INFO, TYPE = retrieve_snp_fields_from_gen_file(line1, line2)
+            Genotype = find_best_genotype(line1)
             if INFO >= float(threeshold) and (TYPE == 2 or TYPE == 3):
-                dict_known[pos+":"+ref+":"+alt] = GT
+                dict_known[pos+":"+ref+":"+alt] = Genotype
                 total_known += 1
             if INFO >= float(threeshold) and (TYPE == 1):
-                dict_well_imputed[pos+":"+ref+":"+alt] = GT
+                dict_well_imputed[pos+":"+ref+":"+alt] = Genotype
                 total_imputed += 1
             if (TYPE == 1):
                 total_impute += 1
@@ -67,27 +129,15 @@ with gzip.open(gen_input_file, "rt") as f1, \
 with gzip.open(ref_file, "rt") as f3, \
      open(out_file, "w") as f4:
         for line in f3:
-            fields = line.rstrip().split()
-            pos = fields[1]
-            ref = fields[3]
-            alt_list = fields[4].split(",")
-            gt_ref = fields[5]
-            gt_alt = fields[6]
+            pos, ref, alt_list, gt_ref, gt_alt = retrieve_ref_vcf_info(line)
             for alt in alt_list:
                 ident = pos+":"+ref+":"+alt
                 if ident in dict_impute.keys():
                     found_impute += 1
                 if ident in dict_known.keys():
-                    GT_a0 = dict_known[ident].split("/")[0]
-                    GT_a1 = dict_known[ident].split("/")[1]
-                    dict_known.pop(ident)
-                    if GT_a0 == gt_ref and GT_a1 == gt_alt:
-                        corrected_known += 1
+                    corrected_known = compare_gt(dict_known, corrected_known, ident, gt_ref, gt_alt)
                 elif ident in dict_well_imputed.keys():
-                    GT_a0 = dict_well_imputed[ident].split("/")[0]
-                    GT_a1 = dict_well_imputed[ident].split("/")[1]
-                    if GT_a0 == gt_ref and GT_a1 == gt_alt:
-                        corrected_imputed += 1
+                    corrected_imputed = compare_gt(dict_well_imputed, corrected_imputed, ident, gt_ref, gt_alt)
         for key in dict_known:
             print(key, file=f4)
 
