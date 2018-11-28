25a1041e657edf8b1de867b287411b64c18e3ce5
alejandro1395 <a.vale_13@icloud.com>
2018-11-28 15:39:34 +0100

    Update final_maps_cM_converted.py

--- MAP_RECOMB/new_maps_big/final_maps_cM_converted.py
+++ MAP_RECOMB/new_maps_big/final_maps_cM_converted.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+
+"""
+This script take the genetic map distances from the fine-scale recombination map from chimp 
+and calculate their rates according to the genetic distances between positions, the rate 
+between two syntenic blocks in the chimp chromosome (MAPPED to human genome) is not calculated
+"""
+
 import sys
 import re
 import gzip
@@ -12,7 +19,23 @@ else:
     sys.exit("The usage should be ./convert_4Ner_to_cM.py \
     Ner_chr Ne out_cM_file")
 
+#VARIABLES
 first_line = True
+
+#FUNCTIONS
+
+"""
+This function keeps the info coming from the map file of distance and position and keeps it
+in a variable to compute the rate substracting it to the following distance and position in
+the file
+"""
+
+def get_previous_pos_and_mapdis_to_calculated_rate(fields_info, effective_size):
+    previous_pos = int(fields_info[0])
+    prev_gen_map_dis = (float(fields_info[1])*100)/(4*effective_size)
+    return previous_pos, prev_gen_map_dis
+    
+#MAIN
 
 with open(Ner_chr, "r") as f, open(out_cM_file, "w") as out_fh:
     for line in f:
@@ -22,18 +45,15 @@ with open(Ner_chr, "r") as f, open(out_cM_file, "w") as out_fh:
             print("{}\t{}\t{}".format("position", "COMBINED_rate(cM/Mb)",
                                       "Genetic_Map(cM)"), file=out_fh)
             first_line = False
-            previous = int(fields[0])
-            prev_gen_map = (float(fields[1])*100)/(4*Ne)
+            previous, prev_gen_map = get_previous_pos_and_mapdis_to_calculated_rate(fields, Ne)
         else:
             position = int(fields[0])
             range = (position - previous)/1000000
             genetic_map = (float(fields[1])*100)/(4*Ne)
             if genetic_map < prev_gen_map:
-                previous = int(fields[0])
-                prev_gen_map = (float(fields[1])*100)/(4*Ne)
+                previous, prev_gen_map = get_previous_pos_and_mapdis_to_calculated_rate(fields, Ne)
             else:
                 real_dist = genetic_map - prev_gen_map
                 rate = abs(real_dist)/range
                 print("{}\t{}".format(previous, rate), file=out_fh)
-                previous = int(fields[0])
-                prev_gen_map = (float(fields[1])*100)/(4*Ne)
+                previous, prev_gen_map = get_previous_pos_and_mapdis_to_calculated_rate(fields, Ne)
