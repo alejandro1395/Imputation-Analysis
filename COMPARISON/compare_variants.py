efa50f830c69cf20a8f16a4ec5ec695fbaa97ebc
alejandro1395 <a.vale_13@icloud.com>
2018-11-28 20:12:06 +0100

    Update compare_variants.py

--- COMPARISON/compare_variants.py
+++ COMPARISON/compare_variants.py
@@ -4,6 +4,11 @@ import sys
 import re
 import gzip
 
+"""
+This script compares the genotypes between a reference vcf of the chimpanzee and the output coming
+from the IMPUTE2 software in order to know the right imputed positions in our data
+"""
+
 if len(sys.argv) == 4:
     ref_input_file = sys.argv[1]
     calculated_genotypes = sys.argv[2]
@@ -12,6 +17,8 @@ else:
     sys.exit("The usage should be ./compare_snps_imputing.py \
     gen_input_file info_input_name ref_file threeshold out_file")
 
+    
+#VARIABLES
 diction = {}
 types = []
 last_pos = ""
@@ -24,35 +31,87 @@ corrected_three = 0
 variant = False
 Nucleotides = ["A", "C", "G", "T"]
 
+#FUNCTIONS
+
+"""
+Function that takes fields from the genotype calculated file coming from impute2 output
+"""
+
+def take_info_from_genotyped_file(line1, dictionary, last_position):
+    fields = line1.rstrip().split()
+    position = fields[0]
+    allele0 = fields[1]
+    allele1 = fields[2]
+    genotype = fields[3]
+    TYPE = int(fields[4])
+    if position != last_position:
+        dictionary[position] = []
+        dictionary[position].append([allele0, allele1, genotype, TYPE])
+    else:
+        dictionary[position].append([allele0, allele1, genotype, TYPE])
+    last_position = position
+    return dictionary, last_position
+
+"""
+Function that takes fields from the reference file coming from the vcf panel after removing it
+and preparing it for comparing rith impute2 result
+"""
+
+def take_info_from_reference_file(line2):
+    fields = line2.rstrip().split()
+    posi = fields[1]
+    refe = fields[3]
+    alte_list = fields[4].split(",")
+    gt_refe = fields[5]
+    gt_alte = fields[6]
+    return posi, refe, alte_list, gt_refe, gt_alte
+
+"""
+Functions that translate impute2 genotypes to vcf 0/1 format for biallelic SNPs found
+with the software
+"""
+
+def translate_to_vcf_genotype(all_gt, refe, counter):
+    for all in all_gt:
+        if refe == all:
+            counter += 1
+    if count == 2:
+        gt_imp_refe = "0"
+        gt_imp_alte = "0"
+    elif count == 1:
+        gt_imp_refe = "0"
+        gt_imp_alte = "1"
+    elif count == 0:
+        gt_imp_refe = "1"
+        gt_imp_alte = "1"
+    return gt_imp_refe, gt_imp_alte
+
+"""
+FUNCTION for suming the genotype count and matches of each type of snp
+determined by IMPUTE2
+"""
+
+def sum_counter_genotype_found(total_count, correct_count, gt_imp_refe, gt_refe, gt_imp_alte, gt_alte, variant):
+    total_count += 1
+    if gt_imp_refe == gt_refe and gt_imp_alte == gt_alte:
+        correct_count += 1
+    variant = False
+    return total_count, correct_count, variant
+
+
+
+#MAIN
+
 with open(calculated_genotypes, "r") as f1:
     next(f1)
     for line in f1:
-        fields = line.rstrip().split()
-        position = fields[0]
-        allele0 = fields[1]
-        allele1 = fields[2]
-        genotype = fields[3]
-        TYPE = int(fields[4])
-        if position != last_pos:
-            diction[position] = []
-            diction[position].append([allele0, allele1, genotype, TYPE])
-        else:
-            diction[position].append([allele0, allele1, genotype, TYPE])
-        last_pos = position
+        diction, last_pos = take_info_from_genotyped_file(line1, diction, last_pos)
 
-
-
-
-
+        
 with gzip.open(ref_input_file, "rt") as f2, \
      open(out_file, "w") as f3:
         for line in f2:
-            fields = line.rstrip().split()
-            pos = fields[1]
-            ref = fields[3]
-            alt_list = fields[4].split(",")
-            gt_ref = fields[5]
-            gt_alt = fields[6]
+            pos, ref, alt_list, gt_ref, gt_alt = take_info_from_reference_file(line2)
             if pos in diction.keys() and len(alt_list) == 1 \
                and alt_list[0] in Nucleotides and ref in Nucleotides \
                and gt_ref != "." and gt_alt != ".":
@@ -62,36 +121,23 @@ with gzip.open(ref_input_file, "rt") as f2, \
                     if ref in (element[0], element[1]) and (element[0] in alt_list \
                                                             or element[1] in alt_list):
                         variant = True
-                        for all in alleles_gt:
-                            if ref == all:
-                                count += 1
-                        if count == 2:
-                            gt_imp_ref = "0"
-                            gt_imp_alt = "0"
-                        elif count == 1:
-                            gt_imp_ref = "0"
-                            gt_imp_alt = "1"
-                        elif count == 0:
-                            gt_imp_ref = "1"
-                            gt_imp_alt = "1"
+                        gt_imp_ref, gt_imp_alt = translate_to_vcf_genotype(alleles_gt, ref, count)
                         if variant and (element[3] == 2):
-                            total_both_count += 1
-                            if gt_imp_ref == gt_ref and gt_imp_alt == gt_alt:
-                                corrected_both_count += 1
-                            variant = False
+                            total_both_count, corrected_both_count, variant = sum_counter_genotype_found(total_both_count, 
+                                                                                                         corrected_both_count, 
+                                                                                                         gt_imp_ref, gt_ref, gt_imp_alt, 
+                                                                                                         gt_alt, variant)
                             break
                         elif variant and (element[3] == 3):
-                            total_three += 1
-                            print(pos, diction[pos], file=f3)
-                            if gt_imp_ref == gt_ref and gt_imp_alt == gt_alt:
-                                corrected_three += 1
-                            variant = False
+                            total_three, corrected_three, variant = sum_counter_genotype_found(total_three, corrected_three, 
+                                                                                                gt_imp_ref, gt_ref, gt_imp_alt, 
+                                                                                                gt_alt, variant)
+
                             break
                         elif variant and (element[3] == 1):
-                            total_imputed += 1
-                            if gt_imp_ref == gt_ref and gt_imp_alt == gt_alt:
-                                corrected_imputed += 1
-                            variant = False
+                            total_imputed, corrected_imputed, variant = sum_counter_genotype_found(total_imputed, corrected_imputed, 
+                                                                                                gt_imp_ref, gt_ref, gt_imp_alt, 
+                                                                                                gt_alt, variant)
                             break
 
 
